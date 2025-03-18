009f44e4fca72a3532e5d9b2bee4c9de2ac65fb6
Hunter Mailloux <huntermmailloux@gmail.com>
2025-03-18 16:32:26 -0400

    CORS Policy update

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export function middleware(req) {
     const res = NextResponse.next();
     
-    res.headers.set("Access-Control-Allow-Origin", "https://www.wsb-analysis.ca");
+    res.headers.set("Access-Control-Allow-Origin", "*");
     res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
